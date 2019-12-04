ecf5a09f49b43a4f13e8a8ef11b41b2c398b1709
Christopher Carr <cycarr@dundee.ac.uk>
2019-12-04 14:45:27 +0000

    documented and removed testing lines

--- bot.py
+++ bot.py
@@ -16,18 +16,16 @@ server = input("Server IP to connect to: ") # Server
 port = 6667
 channel = "#test" # channel to join
 uname = "ProBot" # the bot name
-adminname = "chris" #Your IRC nickname. 
-exitcode = "bye " + uname #exit code for admins to close the bot remotely
 client_socket.connect((server, port)) # Here we connect to the server using the port 6667
 client_socket.send(bytes("USER "+ uname +" "+ uname +" "+ uname + " " + uname + "\n", "UTF-8")) # user information
 client_socket.send(bytes("NICK "+ uname +"\n", "UTF-8")) # assign the nick to the bot
 def joinchan(chan): # join channel(s).
   client_socket.send(bytes("JOIN "+ chan +"\n", "UTF-8")) 
-  ircmsg = ""
-  while ircmsg.find("End of NAMES list") == -1: 
-    ircmsg = client_socket.recv(1024).decode("UTF-8")
-    ircmsg = ircmsg.strip('\n\r')
-    print(ircmsg)
+  ircmsg = "" #a variable to output the server's initial data spew
+  while ircmsg.find("End of NAMES list") == -1: #loops until the data spew is finished
+    ircmsg = client_socket.recv(1024).decode("UTF-8") #decodes and 
+    ircmsg = ircmsg.strip('\n\r')                     #strips the data 
+    print(ircmsg)                                     #then prints it
 
 def ping(): # respond to server Pings.
   client_socket.send(bytes("PONG :pingis\n", "UTF-8"))
@@ -37,47 +35,45 @@ def sendMessage(msg, target=channel): # sends messages to the target.
   client_socket.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
 
 def main():
-  joinchan(channel)
+  joinchan(channel) #send request to join channel "#test" by default
   
   while 1:
-    ircmsg = client_socket.recv(1024).decode("UTF-8")
-    ircmsg = ircmsg.strip('\n\r')
-    print(ircmsg)
-    if ("PRIVMSG" in ircmsg): 
-      splitmsg = ircmsg.split(':')
-      #print("message split 1: "+splitmsg[1])
-      #print("message split 2: "+splitmsg[2])
+    ircmsg = client_socket.recv(1024).decode("UTF-8") #when a message is received we store and decode it
+    ircmsg = ircmsg.strip('\n\r') #strip message of end line chars
+    if ("PRIVMSG" in ircmsg): #checks if the message recieved is a message that should be interpreted
+      splitmsg = ircmsg.split(':') #splits message into parts that will be parsed to form a response
       message = splitmsg[2]
-      if message.__contains__('!'):
+      if message.__contains__('!'): #checks if there is a command in a channel message
         command = message.split('!')
+        command = command[1].split(' ')[0]
         print("Command word is " + command[1])
-        reply(command[1])
-      if (uname in splitmsg[1]):
-        print("I heard my name")
+        reply(command[1]) #responds to the command if it is valid
+      if (uname in splitmsg[1]): #when the bot recieves a direct message
+        print("I've been messaged!")
         recipientName = str(splitmsg[1].split('!')[0])
         replyRandom(recipientName)
     else:
-      if ircmsg.find("PING :") != -1:
-        ping()
+      if ircmsg.find("PING :") != -1: #if the message is a server ping request
+        ping() #reply with pong
         
-            
+###### The functions below are for handing the !commands in channel chat ######            
 def funcTime():
-    output = "The time is... " + strftime("%H:%M:%S", gmtime())
+    output = "The time is " + strftime("%H:%M:%S", gmtime())
     print(output)
     sendMessage(output)
 
 def funcDate():
-    output = "The date is... " + strftime("%d-%m-%Y", gmtime())
+    output = "The date is " + strftime("%d-%m-%Y", gmtime())
     print(output)
     sendMessage(output)
 
 def funcIP():
-    output = "The IP is... " + server
+    output = "The server IP is " + server
     print(output)
     sendMessage(output)
 
 def funcPort():
-    output = "The date is... " + str(port)
+    output = "The server port is " + str(port)
     print(output)
     sendMessage(output)
 
@@ -92,7 +88,7 @@ def funcDayOfWeek():
 
 # ======================================================================================================
 
-def reply(s):
+def reply(s): #replies to channel !commands by parsing the command and calling the correct
     # Print message
     print(f'Command was !{s}')
     
@@ -121,8 +117,8 @@ def reply(s):
         sendMessage('Thats not a valid command')
 
 
-# Add to this list of random replys
-#function to be called to for when a user private messages the bot
+# Add to this list of random replies
+#when a user private messages the bot, picks a random reply from the list below and sends it along to the user that messaged
 def replyRandom(recipient):
     reply_list = [
         'Alright there',
@@ -135,8 +131,8 @@ def replyRandom(recipient):
         'uhh nah',
         'Hold up. Not had coffee yet and I am not ready for this'
     ]
-    reply=random.choice(reply_list)
+    reply=random.choice(reply_list) #randomly selects a message from the list
     print("Sending message "+reply+" to " + recipient)
     sendMessage(reply,recipient)
-main()
+main() #calls the main method
  
